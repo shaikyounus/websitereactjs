343b280835add95c74d601c9e1fde6eaf0607d3f
SHAIK MOHAMMAD YOUNUS <37431701+shaikyounus@users.noreply.github.com>
2020-12-29 15:05:35 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,7 +118,7 @@ module.exports = {
               // Values below are default
               prompt: {
                 user: 'root',
-                host: 'https://www.heroku.com/',
+                host: 'localhost',
                 global: false,
               },
             },
